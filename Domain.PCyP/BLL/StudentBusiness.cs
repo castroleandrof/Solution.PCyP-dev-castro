4682512b66d1bd6b4296988648d29f0e60fd59c1
castroleandrof <castroleandrof@gmail.com>
2017-09-28 20:50:39 -0300

    controller

--- Domain.PCyP/BLL/StudentBusiness.cs
+++ Domain.PCyP/BLL/StudentBusiness.cs
@@ -18,7 +18,7 @@ namespace Domain.PCyP.BLL
             student.CreatedOn = DateTime.Now;
             student.ChangedOn = DateTime.Now;
             char c = student.FirstName.FirstOrDefault();
-            student.Alias = c + model.LastName;
+            student.Alias = c + student.LastName;
             var cdal = new StudentRepository();
             cdal.Add(student);
 
@@ -59,22 +59,31 @@ namespace Domain.PCyP.BLL
 
         public static void Controller()
         {
+            string[] arr_nombre = new string[] { };
+            string[] arr_apellido = new string[] { };
             string line_nombre, line_apellido;
             System.IO.StreamReader file_apellidos = new System.IO.StreamReader(@"D:\leandro.castro\apellidos.txt");
             System.IO.StreamReader file_nombres = new System.IO.StreamReader(@"D:\leandro.castro\nombres.txt");
+            int contador_nombre = 0;
+            int contador_apellido = 0;
             while ((line_nombre = file_nombres.ReadLine()) != null)
             {
-
+                arr_nombre[contador_nombre] = line_nombre;
+                contador_nombre = contador_nombre+1;
             }
                 while ((line_apellido = file_apellidos.ReadLine()) != null)
                 {
-                    Student student = new Student();
-                    student.FirstName = line_nombre;
-                    student.LastName = line_apellido;
-                    Add(student);
-                }
+                arr_nombre[contador_apellido] = line_apellido;
+                contador_apellido=contador_apellido+1;
+
+                //Student student = new Student();
+                //student.FirstName = line_nombre;
+                //student.LastName = line_apellido;
+                //Add(student);
+            }
             }
 
         }
     }
-}
+
+
